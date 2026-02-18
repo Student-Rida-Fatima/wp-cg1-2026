664064feb10760f4e8091a17eb30d23dd112d0b9
Student-Rida-Fatima <ridaf8689@gmail.com>
2026-02-18 03:56:41 -0800

    array

--- js.php
+++ js.php
@@ -10,7 +10,26 @@ else if($number < 0){
     echo "The number is not positive.";
 }
 else{
-    echo "it is not a number";
+    echo "it is not a number" ;
 }
+echo "<br>";
+$person = [
+    "name" => "Ali",
+    "age" => 25,
+    "city" => "Karachi"
+];
+
+print_r($person);
+echo "<br>";
+$cars_obj = [
+   "cars" => "fortuner",
+   "color" => "black",
+    "price" => "2 cr",
+];
+    
+
+
+print_r($cars_obj)
+
 
 ?>
