b964fd753517050acf5ab15778242e83d5842c87
Student-Rida-Fatima <ridaf8689@gmail.com>
2026-02-18 02:57:10 -0800

    php practice if else

---
+++ js.php
@@ -0,0 +1,16 @@
+<?php
+
+$number=-4;
+
+
+if ($number > 0) {
+    echo "The number is positive.";
+} 
+else if($number < 0){
+    echo "The number is not positive.";
+}
+else{
+    echo "it is not a number";
+}
+
+?>
